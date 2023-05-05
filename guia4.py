dfea2b663cce6952870c5cc6ec69e966c8e99c9d
Yajarufe <yjarufea@udd.cl>
2023-05-05 17:19:26 -0400

    seagrego

--- guia4.py
+++ guia4.py
@@ -1,11 +1,11 @@
 # Guia 4) Tutotial de como ajustar distribuciones en python.(traduccion r a python)
-
+#%%
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import bernoulli,norm
-
+#%%
 
 n= 100
 
@@ -18,14 +18,38 @@ sns.scatterplot(data=h)
 D = np.random.binomial(n=1, p=0.5, size = n)
 histar = h.copy()
 histar[D==1] = np.nan
+#%%
+# Dog east homework of students who study too much
+
+d2 = np.random.binomial(n=1, p=np.where(s>0, 0.8, 0))
+Hstar = h.copy()
+Hstar[D==1] = np.nan
+
+plt.scatter(s, h, color='gray', alpha=0.8, linewidth=2)
+plt.scatter(s, Hstar, color='red', alpha=1, linewidth=3)
+plt.show()
+
+#%%
 
 
 s2 = np.random.normal(size=n)
-h2 = np.random.normal(scale=0.5*abs(s2), size=n)
-d2 = np.random.binomial(n=1, p=np.where(s2>0, 0.8, 0))
+h2 = np.random.normal(loc=1-np.exp(-0.7*s2), size=n)
+d2 = np.random.binomial(n=1, p=np.where(s2>0, 0.1, 0))
 Hstar = h2.copy()
 Hstar[d2==1] = np.nan
 
 plt.scatter(s2, h2, color='gray', alpha=0.8, linewidth=2)
 plt.scatter(s2, Hstar, color='red', alpha=1, linewidth=3)
 plt.show()
+#%%
+
+d2 = np.random.binomial(n=1, p=np.where(s>0, 0.9, 0))
+Hstar = h.copy()
+Hstar[D==1] = np.nan
+
+plt.scatter(s, h, color='gray', alpha=0.8, linewidth=2)
+plt.scatter(s, Hstar, color='red', alpha=1, linewidth=3)
+plt.show()
+
+
+# %%
